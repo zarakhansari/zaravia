e504b2d0a740a295ba1820106b5f6eb1b1904670
Zahra Khansari <zarakhansari@gmail.com>
2026-09-07 09:51:12 +0200

    feat: add WeatherDay interface and include forecast array in Weather type

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -1,6 +1,13 @@
+export interface WeatherDay {
+  date: string;
+  temperatureMax: number;
+  temperatureMin: number;
+  weatherCode: number;
+}
 
 export interface Weather {
-    temperature: number;
-    windSpeed: number;
-    weatherCode: number;
+  temperature: number;
+  windSpeed: number;
+  weatherCode: number;
+  forecast: WeatherDay[];
 }
